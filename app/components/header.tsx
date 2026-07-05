8e78a0dd85bf33e703c1872571e0cdd099b7f20c
Danny Vasta <djv711@gmail.com>
2026-07-05 14:08:33 -0400

    simplify header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,7 @@
-import { ThemeToggle } from "@/components";
-
 export const Header = () => {
   return (
-    <header className="relative sticky top-0 z-40 flex items-center w-full pt-4 bg-background">
-      <span className="flex items-center gap-2 w-max">
-        <h1>danny vasta</h1>
-        {/* <h1 className="md:hidden">dv</h1> */}
-      </span>
-      {/* <span className="ml-auto">
-        <ThemeToggle />
-      </span> */}
+    <header>
+      <h1>danny vasta</h1>
     </header>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,38 @@ import { PROJECTS } from "@/constants";
 export default function Home() {
   return (
     <main className="grid grid-cols-1 gap-8 sm:grid-cols-3">
-      <article className="flex flex-col gap-2">
+      <article>
         <h2>me</h2>
-        <p>software engineer with 7+ years of experience.</p>
-        <p>
-          fullstack engineer at{" "}
-          <Link href="https://www.chewy.com/" target="_blank" rel="noreferrer">
-            Chewy
-          </Link>{" "}
-          on the Sponsored Ads team.
-        </p>
+        <section className="flex flex-col gap-2">
+          <p>software engineer with 7+ years of experience.</p>
+          <p>
+            fullstack engineer at{" "}
+            <Link
+              href="https://www.chewy.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Chewy
+            </Link>{" "}
+            on the Sponsored Ads team.
+          </p>
+        </section>
       </article>
-      <article className="flex flex-col gap-2">
+      <article>
         <h2>projects</h2>
-        <ul>
-          {PROJECTS.map(({ id, name }) => (
-            <li key={id}>{name}</li>
-          ))}
-        </ul>
+        <section className="flex flex-col gap-2">
+          <ul>
+            {PROJECTS.map(({ id, name }) => (
+              <li key={id}>{name}</li>
+            ))}
+          </ul>
+        </section>
       </article>
-      <article className="flex flex-col gap-2">
+      <article>
         <h2>links</h2>
-        <SocialIcons />
+        <section className="flex flex-col gap-2">
+          <SocialIcons />
+        </section>
       </article>
     </main>
   );

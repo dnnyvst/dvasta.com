54c1253b6d03e930632a9fb770da898eae3a977a
Danny Vasta <djv711@gmail.com>
2026-07-06 17:58:37 -0400

    make ui folder

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,1 @@
-export * from "./header";
-export * from "./aside";
-export * from "./social-icons";
-export * from "./theme-toggle";
-export * from "./modal";
+export * from "./ui";
---
+++ app/components/ui/index.ts
@@ -0,0 +1,5 @@
+export * from "./header";
+export * from "./aside";
+export * from "./social-icons";
+export * from "./theme-toggle";
+export * from "./modal";
